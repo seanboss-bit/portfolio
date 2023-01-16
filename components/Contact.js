d146f909ff5489fc56ec5ffd084774c518722d47
Abubakar Musa <shaunaibek@gmail.com>
2023-01-16 17:13:32 +0100

    some changes

--- components/Contact.js
+++ components/Contact.js
@@ -13,11 +13,11 @@ const Contact = () => {
         <h2 className="py-4 capitalize text-center sm:text-left">
           get in touch
         </h2>
-        <div className="grid lg:grid-cols-2 gap-8">
+        <div className="grid lg:grid-cols-1 gap-8">
           {/* left */}
           <div className="w-full h-full shadow-lg shadow-gray-400 rounded-xl p-4">
             <div className="lg:p-4 h-full">
-              <div>
+              <div className="flex items-center justify-center">
                 <img
                   src="https://images.unsplash.com/photo-1499914485622-a88fac536970?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dHlwaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
                   alt="#"
@@ -25,15 +25,15 @@ const Contact = () => {
                 />
               </div>
               <div>
-                <h2 className="py-2 capitalize">sean</h2>
-                <p className="capitalize">full stack developer</p>
-                <p className="capitalize py-4">
+                <h2 className="py-2 capitalize text-center">sean</h2>
+                <p className="capitalize text-center">full stack developer</p>
+                <p className="capitalize py-4 text-center">
                   i am available for both freelance , partime and full time job
                   opportunities
                 </p>
               </div>
               <div>
-                <p className="uppercase pt-8">connect with me</p>
+                <p className="uppercase pt-8 text-center">connect with me</p>
                 <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
                   <Link
                     href={"https://www.instagram.com/muso_abk/"}
@@ -71,53 +71,6 @@ const Contact = () => {
             </div>
           </div>
           {/* right */}
-          <div className="w-full h-auto shadow-lg lg:p-4 shadow-gray-400 rounded-xl">
-            <div className="p-4">
-              <form>
-                <div className="grid md:grid-cols-2 gap-4 w-full py-2">
-                  <div className="flex flex-col">
-                    <label className="text-sm uppercase py-2">name</label>
-                    <input
-                      type="text"
-                      className="border-2 rounded-lg p-3 flex border-gray-300 focus:outline-none"
-                    />
-                  </div>
-                  <div className="flex flex-col">
-                    <label className="text-sm uppercase py-2">phone</label>
-                    <input
-                      type="text"
-                      className="border-2 rounded-lg p-3 flex border-gray-300 focus:outline-none"
-                    />
-                  </div>
-                </div>
-                <div className="flex flex-col py-2">
-                  <label className="text-sm uppercase py-2">email</label>
-                  <input
-                    type="email"
-                    className="border-2 rounded-lg p-3 flex border-gray-300 focus:outline-none"
-                  />
-                </div>
-                <div className="flex flex-col py-2">
-                  <label className="text-sm uppercase py-2">subject</label>
-                  <input
-                    type="text"
-                    className="border-2 rounded-lg p-3 flex border-gray-300 focus:outline-none"
-                  />
-                </div>
-                <div className="flex flex-col">
-                  <label className="text-sm uppercase py-2">message</label>
-                  <textarea
-                    type="text"
-                    className="border-2 rounded-lg p-3 flex border-gray-300 focus:outline-none"
-                    rows="10"
-                  />
-                </div>
-                <button className="w-full p-2 text-gray-100 mt-4">
-                  send message
-                </button>
-              </form>
-            </div>
-          </div>
         </div>
         <div className="flex justify-center py-12">
           <Link href={"/"}>
